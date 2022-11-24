41d3d051f170e24ffd335c9f976366209a9eb2ba
dedalovitor <dedalovitor@gmail.com>
2022-11-24 19:50:45 +0000

    uploading my code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let end = [".com", ".info", ".es", ".org"];
+  let domainName = [];
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < end.length; e++) {
+          domainName.push(" " + pronoun[p] + adj[a] + noun[n] + end[e]);
+        }
+      }
+    }
+  }
+  console.log(domainName);
+  document.getElementById("domain.results").innerHTML = domainName;
 };
